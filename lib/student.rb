9d10ffd7c2c58ceb1bb029e1e8a210b7e50745d6
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 05:34:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,13 +79,13 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X(num = 10)
+  def self.all_students_in_grade_X(num)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = ?;
       SQL
     DB[:conn].execute(sql, num).map do |row|
-      self.new_from_db
+      self.new_from_db(row)
     end
   end
 
