fde2347ede3cd245ce949af0299bae2f3b1bcaad
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 05:21:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT COUNT(name) FROM students
+      SELECT * FROM students
       WHERE grade = 9
       SQL
     DB[conn:].execute(sql)
