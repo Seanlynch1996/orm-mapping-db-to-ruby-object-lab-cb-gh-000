c30f0054acce1ad4e8209bcc78dacb3116736f0d
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 05:05:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,9 +17,7 @@ class Student
       SELECT *
       FROM students
       SQL
-    DB[:conn].execute(sql).map |row|
-      self.new_from_db(row)
-    end
+    #end
   end
 
   def self.find_by_name(name)
