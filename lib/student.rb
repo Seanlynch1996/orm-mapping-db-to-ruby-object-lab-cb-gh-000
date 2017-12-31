50d1adbdeb0fc661d0a5a8ed2623cf457f65a9af
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 05:29:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
     end
   end
 
-  def first_student_in_grade_10
+  def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
@@ -78,7 +78,7 @@ class Student
       self.new_from_db(row)
     end
   end
-  
+
 
 
   def save
